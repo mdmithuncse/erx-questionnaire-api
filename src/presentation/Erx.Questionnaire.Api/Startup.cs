98dd906854251ff3ce0a609fe15f77d9441cfd63
mdmithuncse <nuruzzaman@7peakssoftware.com>
2021-08-11 21:11:23 +0700

    change swagger title text

--- src/presentation/Erx.Questionnaire.Api/Startup.cs
+++ src/presentation/Erx.Questionnaire.Api/Startup.cs
@@ -30,7 +30,7 @@ namespace Erx.Questionnaire.Api
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Erx.Questionnaire.Api", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Erx Questionnaire Api", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Erx.Questionnaire.Api
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Erx.Questionnaire.Api v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Erx Questionnaire Api v1"));
             }
 
             app.UseHttpsRedirection();
